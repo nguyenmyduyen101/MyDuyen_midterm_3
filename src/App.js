1ab5f00ab3cc6e484331554f86f21f18648c366b
nguyenmyduyen101 <136870985+nguyenmyduyen101@users.noreply.github.com>
2024-06-06 14:22:28 +0700

    Create About and NotFound components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Navbar />
         <div className="container">
-          <h1>GitHub Users Data</h1>
           <Switch>
             <Route exact path="/" component={Search} />
+            <Route exact path="/about" component={About} />
+            <Route path="/*" component={NotFound}></Route>
           </Switch>
         </div>
       </Router>
