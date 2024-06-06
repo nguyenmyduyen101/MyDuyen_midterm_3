5ac63eba94be528195b6ed9e15970fda7ea3ee6b
nguyenmyduyen101 <136870985+nguyenmyduyen101@users.noreply.github.com>
2024-06-06 10:14:01 +0700

    Use useEffect to fetch data with axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import axios from "axios";
+import { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 function App() {
-return (
-<div className="App">
-<Navbar />
-<div className="container">
-<h1>Hello from React</h1>
-</div>
-</div>
-);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://api.github.com/users");
+        console.log("GitHub Users:", response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []); 
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <h1>Hello from React</h1>
+      </div>
+    </div>
+  );
 }
 export default App;
